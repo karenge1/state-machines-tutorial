7768bd2fc67cc80a4a6fb95388ed4958b0d19296
Karen Ge <31216127+karenge1@users.noreply.github.com>
2017-08-30 12:04:25 -0500

    brick works!

--- Hello.java
+++ Hello.java
@@ -5,9 +5,12 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+import lejos.nxt.Button;
+
 public class Hello
 {
     public static void main (String[] args) {
         System.out.println("Hello World");
+        Button.waitForAnyPress();
     }
 }
